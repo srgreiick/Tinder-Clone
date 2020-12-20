f9d902d3cd5ab1606aff252485b7ccc9871aafbd
srgreiick <samgreilick@gmail.com>
2020-12-20 13:52:16 -0700

    logos and styleing

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,23 @@
 import React from 'react'
-// import "/Header.css"
+import "./Header.css"
+import PersonIcon from '@material-ui/icons/Person';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header(){
     return(
         <div className='header'>
-            <h2>I am Header</h2>
+            <IconButton>
+            <PersonIcon fontSize="large" className="header_icon"/>
+            </IconButton>
+
+        <img
+        className="header_logo"
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="tinder-ico"
+        />
+
+
+
         </div>
     )
 }
